19e3170b71ef7599eb29e35b5ec31cd13f9f43e3
Endki <khb3221@naver.com>
2020-04-23 22:44:57 +0900

    200423

--- sample/ch02/js/ui.js
+++ sample/ch02/js/ui.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
-	
+    $("#container").addClass("start");
+	$("nav li").click(function(){
+        $("#container").css("max-width","100%");
+        var id=$(this).attr("data-rol");
+        // alert("click event");
+        $("nav li").removeClass("on");
+        $(this).addClass("on");
+        $(".content").removeClass("prev this next");
+        $(".content").css("width","100%");
+        $("#"+id).addClass("this").prevAll().addClass("prev");
+        // $("#"+id).addClass("this").css("max-width","100%");
+        $("#"+id).nextAll().addClass("next");
+        // alert($(this).attr("data-rol"));
+    });
+    $(".logo_box").click(function(){
+        $("nav li").removeClass("on");
+        $(".content").removeClass("prev this next");
+        // $(".content").css("width","25%");
+        $("#container").css("max-width","1200px");
+    });
 });
 
